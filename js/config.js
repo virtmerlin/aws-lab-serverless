429a4948cbc3656c779314a673f4110cafcbd8b0
Glynn <mglynn@a483e737f212.ant.amazon.com>
2019-10-24 10:19:42 -0500

    Add cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_PRNYnFY63', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6c1n5gt77jk6q94gpknmt3ihd0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
